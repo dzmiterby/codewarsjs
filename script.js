3818f3c1808bff24d2339773acfe514905e38a01
100nout.by <dzmiter2105@gmail.com>
2023-06-14 09:21:09 +0300

    js 209

--- script.js
+++ script.js
@@ -1922,3 +1922,10 @@ function addLength(str) {
     }
     return arr;
 }
+
+// =============== 209 What's the real floor?
+function getRealFloor(n) {
+    if (n <= 0) return n;
+    if (n > 0 && n < 13) return n - 1;
+    if (n > 13) return n - 2;
+}
